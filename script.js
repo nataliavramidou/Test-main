50fd121df601a66f54a63755839ca3496b46ce8b
nataliavramidou <nata_av11@hotmail.com>
2024-11-11 13:11:16 +0200

    Add files via upload
    first commiy

---
+++ script.js
@@ -0,0 +1,27 @@
+var menu = document.querySelector(".menu");
+var header = document.querySelector(".header");
+var navItems = document.querySelectorAll(".main-nav-item");
+
+menu.addEventListener("click", () => {
+  header.classList.toggle("open");
+});
+
+console.log(navItems);
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    header.classList.remove("open");
+  });
+});
+
+var headings = document.querySelectorAll(".heading");
+var infoCols = document.querySelectorAll(".info-col");
+
+headings.forEach((heading) => {
+  heading.addEventListener("click", () => {
+    headings.forEach((head) => {
+      if (head != heading) head.parentNode.classList.remove("active");
+    });
+
+    heading.parentNode.classList.toggle("active");
+  });
+});
